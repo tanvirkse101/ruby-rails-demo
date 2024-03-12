f37ecd8ce30e6917e5641482e117d63b1f65c924
tanvirkse <tanvirkse@gmail.com>
2024-03-12 09:42:51 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,9 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
-  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
   has_many :active_relationships,  class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
@@ -20,6 +16,12 @@ class User < ApplicationRecord
   has_many :groups, through: :group_memberships
   has_many :chats, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
