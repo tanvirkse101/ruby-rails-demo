cd201e57fe4d63a973931f39733a4358ba56e86d
tanvirkse <tanvirkse@gmail.com>
2024-03-12 09:51:31 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://ruby-rails-demo.onrender.com'
+  host = 'https://mysite.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Allow render as a host
   config.hosts.clear
-  config.hosts << "ruby-rails-demo.onrender.com"
+  config.hosts << "mysite.onrender.com"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
